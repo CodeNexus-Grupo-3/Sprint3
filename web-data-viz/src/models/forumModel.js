224f43f475492d5f3fea75f5695780dd7e18dd30
LesleyOliveira <lesley.oliveira@sptech.school>
2026-06-09 18:19:40 -0300

    corrigindo funcao de like

--- web-data-viz/src/models/forumModel.js
+++ web-data-viz/src/models/forumModel.js
@@ -1,7 +1,79 @@
 
+// var db = require("../database/config");
+
+// function listarPosts(fkEquipe) {
+//     var sql = `
+//         SELECT 
+//             pf.idPostagensForum,
+//             pf.titulo,
+//             pf.conteudo,
+//             pf.likes,
+//             pf.dataHora,
+//             pf.fkUsuario
+//         FROM PostagensForum pf
+//         INNER JOIN Usuario u ON u.idUsuario = pf.fkUsuario
+//         WHERE u.fkEquipe = ${fkEquipe}
+//         ORDER BY pf.dataHora DESC
+//     `;
+//     return db.executar(sql);
+// }
+
+// function postarMensagem(titulo, conteudo, fkUsuario) {
+//     var sql = `
+//         INSERT INTO PostagensForum (titulo, conteudo, fkUsuario)
+//         VALUES ('${titulo}', '${conteudo}', ${fkUsuario})
+//     `;
+//     return db.executar(sql);
+// }
+
+// function curtirPost(idPostagensForum) {
+//     var sql = `
+//         UPDATE PostagensForum
+//         SET likes = likes + 1
+//         WHERE idPostagensForum = ${idPostagensForum}
+//     `;
+//     return db.executar(sql);
+// }
+
+// function descurtirPost(idPostagensForum) {
+//     var sql = `
+//         UPDATE PostagensForum
+//         SET likes = likes - 1
+//         WHERE idPostagensForum = ${idPostagensForum}
+//     `;
+//     return db.executar(sql);
+// }
+
+// function deletarPost(idPostagensForum) {
+//     var sql = `
+//         DELETE FROM PostagensForum
+//         WHERE idPostagensForum = ${idPostagensForum}
+//     `;
+//     return db.executar(sql);
+// }
+
+// function editarPost(idPostagensForum, conteudo) {
+//     var sql = `
+//         UPDATE PostagensForum
+//         SET conteudo = '${conteudo}'
+//         WHERE idPostagensForum = ${idPostagensForum}
+//     `;
+//     return db.executar(sql);
+// }
+
+// module.exports = {
+//     listarPosts,
+//     postarMensagem,
+//     curtirPost,
+//     descurtirPost,
+//     deletarPost,
+//     editarPost
+// };
+
+
 var db = require("../database/config");
 
-function listarPosts(fkEquipe) {
+function listarPosts(fkEquipe, fkUsuario) {
     var sql = `
         SELECT 
             pf.idPostagensForum,
@@ -9,9 +81,13 @@ function listarPosts(fkEquipe) {
             pf.conteudo,
             pf.likes,
             pf.dataHora,
-            pf.fkUsuario
+            pf.fkUsuario,
+            CASE WHEN lf.fkUsuario IS NOT NULL THEN 1 ELSE 0 END AS curtido
         FROM PostagensForum pf
         INNER JOIN Usuario u ON u.idUsuario = pf.fkUsuario
+        LEFT JOIN LikesForum lf 
+            ON lf.fkPostagem = pf.idPostagensForum 
+            AND lf.fkUsuario = ${fkUsuario}
         WHERE u.fkEquipe = ${fkEquipe}
         ORDER BY pf.dataHora DESC
     `;
@@ -26,30 +102,44 @@ function postarMensagem(titulo, conteudo, fkUsuario) {
     return db.executar(sql);
 }
 
-function curtirPost(idPostagensForum) {
+function curtirPost(idPostagensForum, fkUsuario) {
     var sql = `
-        UPDATE PostagensForum
-        SET likes = likes + 1
-        WHERE idPostagensForum = ${idPostagensForum}
+        INSERT INTO LikesForum (fkUsuario, fkPostagem)
+        VALUES (${fkUsuario}, ${idPostagensForum})
     `;
-    return db.executar(sql);
+    return db.executar(sql)
+    .then(function() {
+        return db.executar(`
+            UPDATE PostagensForum
+            SET likes = likes + 1
+            WHERE idPostagensForum = ${idPostagensForum}
+        `);
+    });
 }
 
-function descurtirPost(idPostagensForum) {
+function descurtirPost(idPostagensForum, fkUsuario) {
     var sql = `
-        UPDATE PostagensForum
-        SET likes = likes - 1
-        WHERE idPostagensForum = ${idPostagensForum}
+        DELETE FROM LikesForum
+        WHERE fkUsuario = ${fkUsuario} AND fkPostagem = ${idPostagensForum}
     `;
-    return db.executar(sql);
+    return db.executar(sql)
+    .then(function() {
+        return db.executar(`
+            UPDATE PostagensForum
+            SET likes = likes - 1
+            WHERE idPostagensForum = ${idPostagensForum}
+        `);
+    });
 }
 
 function deletarPost(idPostagensForum) {
-    var sql = `
-        DELETE FROM PostagensForum
-        WHERE idPostagensForum = ${idPostagensForum}
-    `;
-    return db.executar(sql);
+    return db.executar(`DELETE FROM LikesForum WHERE fkPostagem = ${idPostagensForum}`)
+    .then(function() {
+        return db.executar(`
+            DELETE FROM PostagensForum
+            WHERE idPostagensForum = ${idPostagensForum}
+        `);
+    });
 }
 
 function editarPost(idPostagensForum, conteudo) {
--- web-data-viz/src/routes/forum.js
+++ web-data-viz/src/routes/forum.js
@@ -1,8 +1,21 @@
+// var express = require("express");
+// var router = express.Router();
+// var forumController = require("../controllers/forumController");
+
+// router.get("/listar/:fkEquipe", forumController.listarPosts);
+// router.post("/postar", forumController.postarMensagem);
+// router.put("/curtir/:id", forumController.curtirPost);
+// router.put("/descurtir/:id", forumController.descurtirPost);
+// router.delete("/deletar/:id", forumController.deletarPost);
+// router.put("/editar/:id", forumController.editarPost);
+
+// module.exports = router;
+
 var express = require("express");
 var router = express.Router();
 var forumController = require("../controllers/forumController");
 
-router.get("/listar/:fkEquipe", forumController.listarPosts);
+router.get("/listar/:fkEquipe/:fkUsuario", forumController.listarPosts);
 router.post("/postar", forumController.postarMensagem);
 router.put("/curtir/:id", forumController.curtirPost);
 router.put("/descurtir/:id", forumController.descurtirPost);
