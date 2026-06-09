a4578866f7aa8cf88e5a97fc0f3ed9f67773f436
LesleyOliveira <lesley.oliveira@sptech.school>
2026-06-09 18:23:21 -0300

    corrigindo o script jsForum

--- web-data-viz/src/routes/forum.js
+++ web-data-viz/src/routes/forum.js
@@ -1,15 +1,4 @@
-// var express = require("express");
-// var router = express.Router();
-// var forumController = require("../controllers/forumController");
 
-// router.get("/listar/:fkEquipe", forumController.listarPosts);
-// router.post("/postar", forumController.postarMensagem);
-// router.put("/curtir/:id", forumController.curtirPost);
-// router.put("/descurtir/:id", forumController.descurtirPost);
-// router.delete("/deletar/:id", forumController.deletarPost);
-// router.put("/editar/:id", forumController.editarPost);
-
-// module.exports = router;
 
 var express = require("express");
 var router = express.Router();
